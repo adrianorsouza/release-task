d3c74083804d04acaa062b8e7541f6de9402cc24
Adriano Rosa <info@adrianorosa.com>
2020-04-15 14:14:10 -0300

    bump.js now uses async Function instead return new Promise

--- src/tasks/bump.js
+++ src/tasks/bump.js
@@ -3,36 +3,32 @@ const path = require('path');
 const semver = require('semver');
 const { writeLog } = require('../helpers');
 
-module.exports = function bump(version, options) {
+module.exports = async function bump(version, options) {
   writeLog('TASK bump');
 
-  return new Promise((resolve, reject) => {
-    const result = [];
-    options.bumpFiles.forEach((file) => {
-      const filePath = path.resolve(file);
+  const result = [];
+  options.bumpFiles.forEach((file) => {
+    const filePath = path.resolve(file);
 
-      if (!fs.existsSync(filePath)) {
-        reject(`Bump ${file} does not exists ${filePath}`);
-        return;
-      }
+    if (!fs.existsSync(filePath)) {
+      throw new Error(`Bump ${file} does not exists ${filePath}`);
+    }
 
-      // bump the file version
-      const data = require(path.resolve(file));
+    // bump the file version
+    const data = require(path.resolve(file));
 
-      if (!data.version || !semver.valid(data.version)) {
-        reject(
-          `Bump file ${file} was not update due invalid version: "${data.version}"`
-        );
-        return;
-      }
+    if (!data.version || !semver.valid(data.version)) {
+      throw new Error(
+        `Bump file ${file} was not update due invalid version: "${data.version}"`
+      );
+    }
 
-      data.version = version;
-      const content =
-        JSON.stringify(data, null, options.config.indentSize) + '\n';
-      fs.writeFileSync(filePath, content, 'utf8');
-      result.push(file);
-    });
-
-    resolve(`TASK bump done! ${result.join(` | `)}\n`);
+    data.version = version;
+    const content =
+      JSON.stringify(data, null, options.config.indentSize) + '\n';
+    fs.writeFileSync(filePath, content, 'utf8');
+    result.push(file);
   });
+
+  return `TASK bump done! ${result.join(` | `)}\n`;
 };
